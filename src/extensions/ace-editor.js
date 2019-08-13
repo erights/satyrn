eb39e84b63fb7cf03a60e1436d206de66aada50e
wip-abramson <wip.abramson@gmail.com>
2019-08-13 23:37:51 +0100

    fix ace editor bug

--- src/extensions/ace-editor.js
+++ src/extensions/ace-editor.js
@@ -11,14 +11,14 @@ showdown.extension('aceEditor', () => {
       replace: function(s, match) {
         content.push(match);
         var n = content.length - 1;
-        return '%EDITOR' + n + '%';
+        return '%EDITOR' + n + '%\n';
       }
     },
     {
       type: 'output',
       filter: function (text) {
         for (var index=0; index< content.length; ++index) {
-          var pat = '<p>%EDITOR' + index + '% *<\/p>';
+          var pat = '%EDITOR' + index + '%';
           text = text.replace(new RegExp(pat, 'gi'), state.getEditorHtml(content[index], index));
           state.editors[index] = null;
         }
